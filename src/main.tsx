2cbdf7ab725c7da77e5373c4e61657923e67d2d6
TerraFit <inquiry@aemaragroup.com>
2026-02-12 11:15:57 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from '/src/App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
-const root = ReactDOM.createRoot(rootElement);
 
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
