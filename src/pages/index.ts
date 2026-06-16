fed37eab932bc3b31f3f5e062da446930bfd3153
TerraFit <inquiry@aemaragroup.com>
2026-06-16 08:53:19 +0200

    Create index.ts to export tab components

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+// src/pages/tabs/index.ts
+export { OverviewTab } from './OverviewTab';
+export { CheckinsTab } from './CheckinsTab';
+export { ReportsTab } from './ReportsTab';
+export { SettingsTab } from './SettingsTab';
