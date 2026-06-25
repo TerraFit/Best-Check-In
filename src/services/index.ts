0a7c0823bc427f5a38e45060dc1a67b8ef549960
TerraFit <inquiry@aemaragroup.com>
2026-06-25 14:39:35 +0200

    Create index.ts to export service modules

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+// src/services/index.ts
+export * from './analyticsService';
+export * from './analyticsAggregator';
+export * from './cityAutocompleteService';
+export * from './countryRegionService';
+export * from './entitlementService';
+export * from './geminiService';
+export * from './geoLoader';
+export * from './importService';
+export * from './regionService';
+export * from './visitorOriginAdapter'; // ✅ NEW
