be2981e080e97cfc54610fd01068d4314c321033
TerraFit <inquiry@aemaragroup.com>
2026-07-09 16:02:08 +0200

    Create index.ts to export staff components

---
+++ src/components/staff/index.ts
@@ -0,0 +1,7 @@
+// src/components/staff/index.ts
+export { StaffPortalWrapper } from './StaffPortalWrapper';
+export { BusinessOverviewTab } from './BusinessOverviewTab';
+export { GuestDietariesTab } from './GuestDietariesTab';
+export { EmployeeManagementTab } from './EmployeeManagementTab';
+export { AuditTrailTab } from './AuditTrailTab';
+export { ResortSettingsTab } from './ResortSettingsTab';
