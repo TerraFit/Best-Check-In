313442fcca689c373d752e67a09e86d47551913b
TerraFit <inquiry@aemaragroup.com>
2026-07-12 20:31:50 +0200

    Change Logo import to default import syntax

--- src/pages/EmployeeLogin.tsx
+++ src/pages/EmployeeLogin.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { Logo } from '../components/Logo';
+import Logo from '../components/Logo';
 import { Phone, Lock, Eye, EyeOff } from 'lucide-react';
 
 export default function EmployeeLogin() {
