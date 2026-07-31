efb7a0fa34b818448cc5a40bc024ebfd2fb6f581
TerraFit <inquiry@aemaragroup.com>
2026-07-31 13:09:41 +0200

    feat(lost-found): wire LostFoundTab into business & employee dashboards

--- src/pages/tabs/index.ts
+++ src/pages/tabs/index.ts
@@ -3,3 +3,4 @@ export { OverviewTab } from './OverviewTab';
 export { CheckinsTab } from './CheckinsTab';
 export { ReportsTab } from './ReportsTab';
 export { SettingsTab } from './SettingsTab';
+export { default as LostFoundTab } from './LostFoundTab';
