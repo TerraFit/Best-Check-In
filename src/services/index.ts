cae1391517aed10327e86ace510dcb9a5443de48
TerraFit <inquiry@aemaragroup.com>
2026-07-31 12:31:59 +0200

    merge: resolve services/index.ts as union of main + room-operations exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
+// src/services/index.ts
+// Union of main (commercial) + feature/room-operations exports.
 export * from './analyticsAggregator';
 export * from './analyticsService';
 export * from './checkinService';
@@ -5,13 +7,14 @@ export * from './cityAutocompleteService';
 export * from './countryRegionService';
 export * from './entitlementService';
 export * from './exportService';
+export * from './featureAccessService';
 export * from './geminiService';
 export * from './geoLoader';
 export * from './grammarlyService';
 export * from './importService';
 export * from './locationIntelligenceService';
 export * from './regionService';
-export * from './visitorOriginAdapter';
 export * from './roomApi';
 export * from './roomDisplayService';
 export * from './roomStatusService';
+export * from './visitorOriginAdapter';
